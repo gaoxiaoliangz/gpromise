9117a3f0e7b661bfb9b27b44fb391462dab6d046
Liang <gaoxiaoliangz@users.noreply.github.com>
2017-08-20 22:47:43 +0800

    2.2.3 2.2.4 2.2.5 passed

--- test/step.js
+++ test/step.js
@@ -5,3 +5,7 @@ require('./aplus-tests/2.1.2');
 require('./aplus-tests/2.1.3');
 require('./aplus-tests/2.2.1');
 require('./aplus-tests/2.2.2');
+require('./aplus-tests/2.2.3');
+require('./aplus-tests/2.2.4');
+require('./aplus-tests/2.2.5');
+// require('./aplus-tests/2.2.6');
