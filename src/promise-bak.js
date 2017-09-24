ef44ea0169c9846ac4d5f1ce8d36435dc8f63938
Liang <gaoxiaoliangz@users.noreply.github.com>
2017-09-24 15:39:34 +0800

    fix throw in handler

--- src/promise-bak.js
+++ src/promise-bak.js
@@ -3,10 +3,6 @@ const RESOLVED = 1
 const REJECTED = 2
 const INTERNAL = () => { }
 
-function isPromise(promise) {
-  return (typeof promise === 'object') && (typeof promise.then === 'function')
-}
-
 /**
  * 等待所有 Promise 依赖的所有 Promise 变为 resolved 或者其中一个 rejected
  * （如果一个 Promise resolve 的值是一个状态为 pending 的 Promise 那么，这个
@@ -15,17 +11,25 @@ function isPromise(promise) {
  * @param {function} done
  */
 function untilFullfill(promise, done) {
-  if (isPromise(promise)) {
-    promise
-      .then(data => {
-        if (isPromise(data)) {
-          untilFullfill(data, done)
-        } else {
-          done(RESOLVED, data)
-        }
-      }, err => {
-        done(REJECTED, err)
-      })
+  // todo: work with other implementations of Promise
+  if (promise instanceof GPromise) {
+    if (promise.state === PENDING) {
+      promise
+        .then(data => {
+          if (data instanceof GPromise) {
+            untilFullfill(data, done)
+          } else {
+            done(promise)
+          }
+        }, err => {
+          // todo: 为什么这边需要重新设置？
+          promise.value = err
+          promise.state = REJECTED
+          done(promise)
+        })
+    } else {
+      done(promise)
+    }
   } else {
     throw new Error('Not a promise!')
   }
@@ -39,61 +43,47 @@ function doAsync(fn) {
 
 function resolveChained(promise) {
   const state = promise.state
-  const value = promise.value
 
-  // if (!promise.queue.length === 0) {
-  //   if (promise.state === REJECTED) {
-  //     return console.error('UnhandledPromiseRejectionWarning:', promise.value)
-  //   }
-  //   return
-  // }
+  if (!promise.queue.length === 0) {
+    if (promise.state === REJECTED) {
+      return console.error('UnhandledPromiseRejectionWarning:', promise.value)
+    }
+    return
+  }
 
   if (state !== RESOLVED && state !== REJECTED) {
     return console.error(`Unexpected error! state should not be ${state}`)
   }
 
-  const settle = (item, handler, done) => {
-    let settleValue = value
-    let settleState = state
-    try {
-      if (typeof handler === 'function') {
-        settleValue = handler(value)
-      }
-    } catch (error) {
-      settleValue = error
-      settleState = REJECTED
-    }
-
-    if (isPromise(settleValue)) {
-      untilFullfill(settleValue, (state2, value2) => {
-        const finalState = settleState === REJECTED ? REJECTED : state2
-        settlePromise(item, finalState, value2)
-        done()
+  const fnName = state === RESOLVED ? 'onFulfilled' : 'onRejected'
+  const processResult = (result2, next) => {
+    if (result2 instanceof GPromise) {
+      untilFullfill(result2, fullfilledPromise => {
+        next.value = fullfilledPromise.value
+        next.state = fullfilledPromise.state
+        resolveChained(next)
       })
     } else {
-      settlePromise(item, settleState, settleValue)
-      done()
+      next.value = result2
+      next.state = RESOLVED
+      resolveChained(next)
     }
   }
 
-  promise.queue.forEach((item) => {
+  promise.queue.forEach((next) => {
     doAsync(() => {
-      if (state === RESOLVED) {
-        settle(item, item.onFulfilled, () => {
-          resolveChained(item)
-        })
-      } else {
-        settle(item, item.onRejected, () => {
-          resolveChained(item)
-        })
+      let result = promise.value
+      if (typeof next[fnName] === 'function') {
+        try {
+          result = next[fnName](promise.value)
+        } catch (error) {
+          
+        }
       }
+
+      processResult(result, next)
     })
   })
-}
-
-function settlePromise(promise, state, value) {
-  promise.state = state
-  promise.value = value
 }
 
 // proto fns using bind
@@ -104,30 +94,15 @@ function registerChained(onFulfilled, onRejected) {
     promise = new GPromise(INTERNAL)
   }
 
-  const getValue = (initValue, handler) => {
-    let value = initValue
-    let errored = false
-    try {
-      if (typeof handler === 'function') {
-        value = handler(value)
-      }
-    } catch (error) {
-      errored = true
-      value = error
-    }
-    return { value, errored }
-  }
-
   if (this.state === RESOLVED) {
     // this will start the new chain reaction
-    promise = new GPromise((resolve, reject) => {
+    promise = new GPromise(resolve => {
       doAsync(() => {
-        const { value, errored } = getValue(this.value, onFulfilled)
-        if (errored) {
-          reject(value)
-        } else {
-          resolve(value)
+        let val = this.value
+        if (typeof onFulfilled === 'function') {
+          val = onFulfilled(this.value)
         }
+        resolve(val)
       })
     })
   }
@@ -136,15 +111,12 @@ function registerChained(onFulfilled, onRejected) {
     if (onRejected) {
       promise = new GPromise((resolve, reject) => {
         doAsync(() => {
+          let val = this.value
           if (typeof onRejected === 'function') {
-            const { value, errored } = getValue(this.value, onRejected)
-            if (errored) {
-              reject(value)
-            } else {
-              resolve(value)
-            }
+            val = onRejected(this.value)
+            resolve(val)
           } else {
-            reject(this.value)
+            reject(val)
           }
         })
       })
@@ -159,14 +131,23 @@ function registerChained(onFulfilled, onRejected) {
 }
 
 function handleExecutorCallback(value, defaultState) {
-  if (isPromise(value)) {
-    untilFullfill(value, (state, value) => {
-      settlePromise(this, state, value)
-      resolveChained(this)
+  // todo
+  if (value instanceof GPromise) {
+    untilFullfill(value, fullfilledPromise => {
+      this.value = fullfilledPromise.value
+      this.state = fullfilledPromise.state
+
+      if (this.executor !== INTERNAL) {
+        resolveChained(this)
+      }
     })
   } else {
-    settlePromise(this, defaultState, value)
-    resolveChained(this)
+    this.value = value
+    this.state = defaultState
+
+    if (this.executor !== INTERNAL) {
+      resolveChained(this)
+    }
   }
 }
 
@@ -202,6 +183,7 @@ class GPromise {
     this.onRejected = undefined
     this.state = PENDING
     this.executor = executor
+
     executor(resolve.bind(this), reject.bind(this))
   }
 
